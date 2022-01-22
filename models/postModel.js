8826e0971649f644bf2b7bec3d877c89bfd06ee3
Kanwarbir02 <kanwarbir2002@gmail.com>
2022-01-22 21:53:51 +0530

    Mongoose Model

---
+++ models/postModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const postModel = new mongoose.model("Post", postSchema);
+
+module.exports = postModel;
