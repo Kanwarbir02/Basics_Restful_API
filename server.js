6aa93eac58de699766a8889802de5b9b0b429963
Kanwarbir02 <kanwarbir2002@gmail.com>
2022-01-22 21:41:36 +0530

    Mongo connection

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const res = require("express/lib/response");
+const dotenv = require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const mongo_uri = process.env.DATABASE_URL
 
 const app = express();
+
+mongoose.connect(mongo_uri) 
+    .then(() => {
+        console.log("Databse connected succesfully");
+    })
+    .catch((err) => {
+        res.status(200).json(err);
+    })
 
 app.get("/", (req,res) => {
     res.send("Home Page");
